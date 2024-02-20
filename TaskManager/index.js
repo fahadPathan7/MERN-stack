8c9f4306ada1b980d29d21aa7fb2dcce42f4103a
Fahad Pathan <fahadpathan56@gmail.com>
2024-02-20 16:32:24 +0600

    update: updated db connection

--- TaskManager/index.js
+++ TaskManager/index.js
@@ -12,36 +12,37 @@ const dotenv = require('dotenv');
 const todoHandler = require('./routeHandler/todoHandler');
 const userHandler = require('./routeHandler/userHandler');
 
-const app = express();
 dotenv.config();
-app.use(express.json());
+
+console.log('connecting to database...');
 
 // database connection
-mongoose.connect('mongodb+srv://' + process.env.USER + ':' + process.env.PASSWORD + '@cluster0.oxoqi6z.mongodb.net/todomanager?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('database connected');
+mongoose.connect('mongodb+srv://' + process.env.USER + ':' + process.env.PASSWORD + '@cluster0.oxoqi6z.mongodb.net/todomanager?retryWrites=true&w=majority')
+.then(() => {
+    console.log('database connected.');
+
+    const app = express();
+    app.use(express.json());
+
+    // routes
+    app.use('/todo', todoHandler);
+    app.use('/user', userHandler);
+
+    // default error handling middleware
+    const errorHandler = (err, req, res, next) => {
+        if (res.headersSent) {
+            return next(err); // pass the error to the next
+            // error handling middleware
+        }
+        res.status(500).json({ error: err.message });
+        // 500 Internal Server Error
+    }
+
+    app.use(errorHandler);
+
+    app.listen(process.env.PORT, () => {
+        console.log('server is running on port: ' + process.env.PORT);
+    });
 }).catch((error) => {
     console.log('error: ', error);
-});
-
-// routes
-app.use('/todo', todoHandler);
-app.use('/user', userHandler);
-
-// default error handling middleware
-const errorHandler = (err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err); // pass the error to the next
-        // error handling middleware
-    }
-    res.status(500).json({ error: err.message });
-    // 500 Internal Server Error
-}
-
-app.use(errorHandler);
-
-app.listen(5000, () => {
-    console.log('server is listening on port 5000...');
 });
