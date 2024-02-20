d9f865ed96e45ef8b36a73b9b7654c38b1609c37
Fahad Pathan <fahadpathan56@gmail.com>
2024-02-20 17:15:36 +0600

    update: env var modified

--- TaskManager/middlewares/checkLogin.js
+++ TaskManager/middlewares/checkLogin.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const checkLogin = (req, res, next) => {
     const token = req.headers.authorization;
     if (token) {
-        jwt.verify(token, process.env.JWT_secret, (error, decoded) => {
+        jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
             if (error) {
                 res.status(401).json({ message: 'unauthorized' });
             } else {
