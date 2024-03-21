cd21b8229685ec0f5a3533f3a00ed4776f1dd7bf
Fahad Pathan <fahadpathan56@gmail.com>
2024-03-21 21:17:44 +0600

    add: learned mapping to data components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,21 @@
 // this file will be used to handle component rendering
 
-// package import
+// external import
 import React from 'react';
 
-// component import
+// internal import
 import Card from './components/Card.jsx';
+import Data from './data.json'
 
 // app name
 const appName = "Todo App";
 
 function App() {
+  
   return (
     <div>
       <h1 className="headingStyle">{appName}</h1>
-      <Card todoTitle="bazar list" todoDesc="alu peyaj rosun"/>
-      <Card />
-      <Card />
+      {Data.map((item, index) => <Card key={index} todoTitle={item.title} todoDesc={item.desc}/>)}
     </div>
   );
 }
