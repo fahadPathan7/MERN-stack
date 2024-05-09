293057c3b1b7af5f0a8b696dc4d0f9a6c1c0ad63
Fahad Pathan <fahadpathan56@gmail.com>
2024-05-09 20:37:48 +0600

    react: learned to use previous state

--- vite-project/src/components/Hooks/Hooks.jsx
+++ vite-project/src/components/Hooks/Hooks.jsx
@@ -1,12 +1,15 @@
+import { set } from 'mongoose';
 import React from 'react'
 
 function Hooks() {
   const [count, setCount] = React.useState(0)
 
   const handleIncrement = () => {
-    setCount(count + 1);
+    setCount(count => count + 1);
+    // increment with the previous value
+    setCount(count => count + 1);
   }
-  
+
   return (
     <div>
         <h1>{count}</h1>
